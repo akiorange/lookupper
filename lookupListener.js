0c8ff1aa24ab7174cd323981df5c7e94ef88bce3
AkiraAso <44665462+akiorange@users.noreply.github.com>
2020-11-04 23:25:58 +0900

    Fix a typo.

--- lookupListener.js
+++ lookupListener.js
@@ -1,7 +1,7 @@
 // Remove the previously added listener.
 // Without removing it, multiple windows will be opened at the same time.
 // However, it cannot remove the listener added before the extension gets reloaded.
-// Hence, an error occurs caused by a ramining listener, but I can think of any solutions.
+// Hence, an error occurs caused by a ramining listener, but I can't think of any solutions.
 // You can catch the error in handleDblclick function for now.
 if (typeof handleDblclick === 'function') {
     document.removeEventListener('dblclick', handleDblclick);
